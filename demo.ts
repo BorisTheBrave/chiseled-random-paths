d5d8ad7497c4a3457cdc4924f42ba3965078476e
Adam Newgas <boris@boristhebrave.com>
2018-04-28 16:16:34 +0100

    Pretty up the grass a bit

--- demo.ts
+++ demo.ts
@@ -69,6 +69,7 @@ function setupDemo2(imagePath: string)
             walkable = randomConnectedSet(width, height, walkable, count)
         }
 
+        regenPerlin();
 
         // Draw everything
         ctx.fillStyle = "#000000";
@@ -86,6 +87,7 @@ function setupDemo2(imagePath: string)
         {
             if(get(x,y))
             {
+                // Draw a path tile
                 let topLeft = get(x-1, y-1);
                 let top = get(x, y-1);
                 let topRight = get(x+1, y-1);
@@ -115,11 +117,22 @@ function setupDemo2(imagePath: string)
             }
             else
             {
+                // Draw a patch of grass
                 let h = tileSize / 2;
-                ctx.drawImage(grass, 0, 0, h, h, x * tileSize + margin + 0, y * tileSize + margin + 0, h, h);
-                ctx.drawImage(grass, 0, 0, h, h, x * tileSize + margin + h, y * tileSize + margin + 0, h, h);
-                ctx.drawImage(grass, 0, 0, h, h, x * tileSize + margin + 0, y * tileSize + margin + h, h, h);
-                ctx.drawImage(grass, 0, 0, h, h, x * tileSize + margin + h, y * tileSize + margin + h, h, h);
+                let frequency = 4;
+                let depth = 8;
+                let roughness = 0.4;
+
+                for(let dx = 0; dx < 1; dx += 0.5) {
+                    for (let dy = 0; dy < 1; dy += 0.5) {
+                        let p = perlin((x + dx) / frequency, (y + dy) / frequency);
+                        p = Math.floor(p * depth + 1.5) + (Math.random() - 0.5) * roughness;
+                        if(p < 0) p = 0;
+                        if(p > 2) p = 2;
+                        let o = h * p;
+                        ctx.drawImage(grass, o, 0, h, h, (x + dx) * tileSize + margin + 0, (y + dy) * tileSize + margin + 0, h, h);
+                    }
+                }
             }
             if(endpoints && exits[x][y])
             {
@@ -147,4 +160,62 @@ function setupDemo2(imagePath: string)
     return {
         redraw,
     }
+}
+
+// Simple perlin noise for pretty grass
+let gradient: [number, number][][]
+
+function regenPerlin()
+{
+    gradient = [];
+    for(let x=0; x < 10;x++) {
+        gradient[x] = [];
+        for(let y=0; y < 10;y++) {
+            let angle = Math.random();
+            let s = Math.sin(angle);
+            let c = Math.cos(angle);
+            gradient[x][y] = [s, c];
+        }
+    }
+}
+
+
+// Function to linearly interpolate between a0 and a1
+// Weight w should be in the range [0.0, 1.0]
+function lerp(a0: number, a1: number, w: number) {
+    return (1.0 - w)*a0 + w*a1;
+}
+
+// Computes the dot product of the distance and gradient vectors.
+function dotGridGradient(ix: number, iy: number, x: number, y: number): number
+{
+    let dx = x - ix;
+    let dy = y - iy;
+    return (dx*gradient[iy][ix][0] + dy*gradient[iy][ix][1]);
+}
+
+// Compute Perlin noise at coordinates x, y
+function perlin(x: number, y: number) {
+
+    // Determine grid cell coordinates
+    let x0 = Math.floor(x);
+    let x1 = x0 + 1;
+    let y0 = Math.floor(y);
+    let y1 = y0 + 1;
+
+    // Determine interpolation weights
+    // Could also use higher order polynomial/s-curve here
+    let  sx = x - x0;
+    let sy = y - y0;
+
+    // Interpolate between grid point gradients
+    let n0 = dotGridGradient(x0, y0, x, y);
+    let n1 = dotGridGradient(x1, y0, x, y);
+    let ix0 = lerp(n0, n1, sx);
+    n0 = dotGridGradient(x0, y1, x, y);
+    n1 = dotGridGradient(x1, y1, x, y);
+    let ix1 = lerp(n0, n1, sx);
+    let value = lerp(ix0, ix1, sy);
+
+    return value;
 }
